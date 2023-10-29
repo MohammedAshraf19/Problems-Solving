9d8b8c600eb097994f8d9c6801ab8a3c9144ab40
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-10-29 12:16:34 +0200

    Solve Problem 460A-Vasya and Socks

---
+++ CodeForces/460A - Vasya and Socks.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        Kattio s = new Kattio();
+        double n = s.nextDouble();
+        double m = s.nextDouble();
+
+        long sum = 0;
+        int count = 0;
+        while (n != 0)
+        {
+            n--;
+            sum++;
+            count++;
+            if (count % m == 0)
+                n++;
+        }
+        s.println(sum);
+        s.close();
+    }
+    static class Kattio extends PrintWriter {
+
+        private final BufferedReader r;
+        private StringTokenizer st;
+
+        // standard input
+        public Kattio() {
+            this(System.in, System.out);
+        }
+
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // USACO-style file input
+        public Kattio(String problemName) throws IOException {
+            super(problemName + ".out");
+            r = new BufferedReader(new FileReader(problemName + ".in"));
+        }
+
+        // returns null if no more input
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
