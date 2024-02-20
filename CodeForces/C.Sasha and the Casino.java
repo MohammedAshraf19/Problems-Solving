417741cc876f5e28bf055cd527d63079d719a7f4
Mohammed Ashraf <mohamedasharf55@gmail.com>
2024-02-20 08:10:53 +0200

    Solve Problem C.Sasha and the Casino

---
+++ CodeForces/C.Sasha and the Casino.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        Kattio s = new Kattio();
+        int t = s.nextInt();
+        while (t-- > 0)
+        {
+            int k = s.nextInt();
+            int x = s.nextInt();
+            long a = s.nextLong();
+            long lose = 1;
+            for (int i = 1; i < x ; i++)
+            {
+                long count  = (lose / (k - 1) + 1);
+                lose += count;
+                if (lose > a)
+                    break;
+            }
+
+            if (a - lose < 0){
+                s.println("NO");
+            }
+            else if ((a - lose) * k > a )
+                s.println("YES");
+            else
+                s.println("NO");
+        }
+        s.close();
+    }
+    static class Kattio extends PrintWriter {
+
+        private final BufferedReader r;
+        private StringTokenizer st;
+
+        // standard input
+        public Kattio() {
+            this(System.in, System.out);
+        }
+
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // USACO-style file input
+        public Kattio(String problemName) throws IOException {
+            super(problemName + ".out");
+            r = new BufferedReader(new FileReader(problemName + ".in"));
+        }
+
+        // returns null if no more input
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
+
