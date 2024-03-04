20e6c05a21b3f4b61e8bb63b82444023ec207a26
Mohammed Ashraf <mohamedasharf55@gmail.com>
2024-03-04 16:05:37 +0200

    Solve Problem G. Brackets (Hard Version)

---
+++ CodeForces/G.Brackets (Hard Version).java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        Kattio s = new Kattio();
+        Stack<Character> stack = new Stack<>();
+        String st = s.next();
+        boolean correct = true;
+        for (char c : st.toCharArray())
+        {
+            if (c == '[' || c == '{' || c == '(')
+                stack.add(c);
+            else {
+                if (stack.isEmpty()){
+                    correct = false;
+                    break;
+                }
+                else {
+                    char ch = stack.pop();
+                    if(ch == '(' && c != ')')
+                    {
+                        correct = false;
+                        break;
+                    }
+                    else  if(ch == '{' && c != '}')
+                    {
+                        correct = false;
+                        break;
+                    }
+                    else if(ch == '[' && c != ']')
+                    {
+                        correct = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (correct)
+            s.println("yes");
+        else
+            s.println("no");
+        s.close();
+    }
+    static class Kattio extends PrintWriter {
+
+        private final BufferedReader r;
+        private StringTokenizer st;
+
+        // standard input
+        public Kattio() {
+            this(System.in, System.out);
+        }
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // USACO-style file input
+        public Kattio(String problemName) throws IOException {
+            super(problemName + ".out");
+            r = new BufferedReader(new FileReader(problemName + ".in"));
+        }
+
+        // returns null if no more input
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
+
